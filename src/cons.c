9f85408e7a7682604e3e067e6d76bb75a7db8cdf
Vanya Klimenko <k60+git@fmap.me>
2020-03-23 20:36:14 +0300

    fix readme, clean whitespace

--- src/cons.c
+++ src/cons.c
@@ -20,13 +20,14 @@ Cell * cons_copy(Cell * x)
   Cell * cu_y = cons(cu_x->car, NULL);
   Cell * y = cu_y;
   Cell* nx_y; // next yu
-  
+
   do {
     cu_x = cu_x->cdr;
     nx_y = cons(cu_x->car, NULL);
     cu_y->cdr = nx_y;
     cu_y = nx_y;
   } while (cu_x->cdr != NULL);
+
   
   return y;
 }
